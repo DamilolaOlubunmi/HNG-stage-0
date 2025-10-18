53e9725834a694535bb4c554839f878bdbb83d57
Damilola Olubunmi <damilolaolubunmi60@gmail.com>
2025-10-18 16:05:14 +0100

    MY PROFILE CARD commit

---
+++ HNG Stage 0/profileCard.js
@@ -0,0 +1,6 @@
+const timeElement = document.querySelector('#time');
+    function updateTime() {
+      timeElement.textContent = Date.now();
+    }
+    updateTime();
+    setInterval(updateTime, 1000);
